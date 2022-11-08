8539ce693a51bcfd5023a56db35d532e0e36dc74
Akshat <akshatpal18@gmail.com>
2022-11-08 13:09:18 +0530

    fixed

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const app = express()
-// var multer = require('multer');
+const port = process.env.PORT || 3000;
 const country_list = require('./countries')
 const path = require("path")
 const cors = require('cors')
 const uploadRoute = require("./routes/upload")
-const morgan = require('morgan')
+// const morgan = require('morgan')
 //middleware
 app.use(express.json())
 app.use(morgan("dev"))
@@ -20,6 +20,6 @@ app.use("/api/countries", (req, res)=>{
 app.use("/", express.static(path.join(__dirname, "public")))
 
 
-app.listen(80, () => {
+app.listen(port, () => {
   console.log("app running on http://localhost:80")
 })
